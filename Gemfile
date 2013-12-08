6f028f28e2fcf610d5d71a295322f101d864a632
Truong Hoang Dung <checkraiser11@gmail.com>
2013-12-08 00:44:56 -0500

    Add user spec

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ gem "active_model_serializers"
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+gem "devise"
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  #attr_accessor :name
+
+  validates :name, presence: true
+  validates :name, uniqueness: true, if: -> { self.name.present? }
+end
